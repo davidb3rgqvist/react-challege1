f6540c0db1024d060f356c942089da00fc997fdf
David Bergqvist <David.bergqvist@gmail.com>
2024-06-25 08:50:14 +0000

    Search Feature Challenge

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import css from './css/Content.module.css';
-import posts from '../posts.json';
+import savedPosts from '../posts.json';
 import PostItem from './PostItem';
 import Loader from './Loader';
 
@@ -9,27 +9,47 @@ class Content extends Component {
     super(props);
 
     this.state = {
-      isLoaded: false
+      isLoaded: false,
+      posts: [],
+      searchInput: ''
     };
   }
 
   componentDidMount() {
     setTimeout(() => {
-      this.setState({ isLoaded: true });
+      this.setState({ isLoaded: true, posts: savedPosts.savedPosts });
     }, 2000);
   }
 
+  handleSearch = (event) => {
+    const searchInput = event.target.value.toLowerCase();
+    const filteredPosts = savedPosts.savedPosts.filter(post =>
+      post.name.toLowerCase().includes(searchInput)
+    );
+    this.setState({ searchInput, posts: filteredPosts });
+  };
+
   render() {
-    const { savedPosts } = posts;
+    const { isLoaded, posts, searchInput } = this.state;
 
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
           <h1>My Photos</h1>
+          <form>
+            <label htmlFor="searchInput">Search by Artist Name: </label>
+            <input
+              type="search"
+              id="searchInput"
+              value={searchInput}
+              onChange={this.handleSearch}
+            />
+            <h4>Posts Found: {posts.length}</h4>
+          </form>
         </div>
         <div className={css.SearchResults}>
-          {this.state.isLoaded ? (
-            <PostItem savedPosts={savedPosts} />
+          {isLoaded ? (
+            posts.map((post) => <PostItem key={post.title} post={post} />)
           ) : (
             <Loader />
           )}
--- my-app/src/components/PostItem.js
+++ my-app/src/components/PostItem.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import css from './css/PostItem.module.css';
 
-function PostItem(props) {
+function PostItem({ post }) {
+  const { title, name, image, description } = post;
   return (
-      props.savedPosts.map(post => {
-          // Extra task destructuring 
-          const {title, name, image, description} = post
-          return <div className={css.SearchItem} key={title}>
-            <h3><strong>Title:</strong> {title}</h3>
-            <p><strong>Name:</strong> {name}</p>
-            <img src={image} alt="random" />
-            <p><strong>Description:</strong> {description}</p>
-          </div>
-          }
-      )
-  )
+    <div className={css.SearchItem} key={title}>
+      <h3><strong>Title:</strong> {title}</h3>
+      <p><strong>Name:</strong> {name}</p>
+      <img src={image} alt="random" />
+      <p><strong>Description:</strong> {description}</p>
+    </div>
+  );
 }
 
-export default PostItem
+export default PostItem;
